c5bcddf3a6d56d5d94e4e421e28452247e5400e4
devsdocs <id45go@gmail.com>
2025-03-21 01:10:04 +0900

    fix: remove unused export of FfmpegNotFoundException in bunny_dart.dart

--- lib/bunny_dart.dart
+++ lib/bunny_dart.dart
@@ -3,7 +3,6 @@ export 'package:bunny_dart/src/stream/helper/tus/store/interface.dart'
     if (dart.library.io) 'package:bunny_dart/src/stream/helper/tus/store/native.dart';
 export 'package:cross_file/cross_file.dart';
 export 'src/stream/helper/tus/bunny_tus_client.dart' show BunnyTusClient;
-export 'src/stream/helper/video_metadata.dart' hide FfmpegNotFoundException;
 export 'src/stream/library/library.dart'
     show BunnyStreamLibraryHelper, BunnyTUSUpload;
 export 'src/stream/stream.dart' show BunnyStream;
